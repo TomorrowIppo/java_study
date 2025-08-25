14a049a0963614bdff8ddc16fdbe384158c97c81
TomorrowIppo <bjk1624@gmail.com>
2025-08-25 17:21:56 +0900

    feat: control_flow/ForLoopExample.java 구현

---
+++ src/C1_java_basics/control_flow/ForLoopExample.java
@@ -0,0 +1,9 @@
+package C1_java_basics.control_flow;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
