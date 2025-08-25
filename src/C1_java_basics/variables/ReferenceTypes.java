60514903fe4993205446a9640cc6521e19b081ee
TomorrowIppo <bjk1624@gmail.com>
2025-08-25 17:18:29 +0900

    feat: ReferenceTypes.java 구현

---
+++ src/C1_java_basics/variables/ReferenceTypes.java
@@ -0,0 +1,11 @@
+package C1_java_basics.variables;
+
+public class ReferenceTypes {
+    public static void main(String[] args) {
+        String message = "Hello, Java!";
+        int[] numbers = {1, 2, 3};
+
+        System.out.println("String: " + message);
+        System.out.println("Array element: " + numbers[0]);
+    }
+}
